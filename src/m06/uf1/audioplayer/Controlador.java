72b07722bf2c379690b4b445f3e62f0cc71fe9a2
5hinko <se4bzhang@gmail.com>
2019-05-01 18:22:49 +0200

    Instanciar los datos y los listeners

--- src/m06/uf1/audioplayer/Controlador.java
+++ src/m06/uf1/audioplayer/Controlador.java
@@ -3,9 +3,10 @@ package m06.uf1.audioplayer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import javax.swing.*;
 import javazoom.jlgui.basicplayer.BasicPlayerException;
 
-public class Controlador implements ActionListener {
+public class Controlador {
 
     private Vista vista;
     private Audio audio;
@@ -13,35 +14,50 @@ public class Controlador implements ActionListener {
     public Controlador() {
         vista = new Vista();
         audio = new Audio("audios/acdc - hells bells.mp3");
+        
+        //afegirDades();
         afegirListenerBotons();
+        //afegirListeners();
     }
 
     public void afegirListenerBotons() {
-        vista.getPlay().addActionListener(this);
-        vista.getStop().addActionListener(this);
-        vista.getPausa().addActionListener(this);
-        vista.getContinuar().addActionListener(this);
+        vista.getPlay().addActionListener(new ControladorBotones());
+        vista.getStop().addActionListener(new ControladorBotones());
+        vista.getPausa().addActionListener(new ControladorBotones());
+        vista.getContinuar().addActionListener(new ControladorBotones());
     }
 
-    //Dotem de funcionalitat als botons
-    public void actionPerformed(ActionEvent esdeveniment) {
-        //Declarem el gestor d'esdeveniments
-        Object gestorEsdeveniments = esdeveniment.getSource();
-        try {
-            if (gestorEsdeveniments.equals(vista.getPlay())) { //Si hem pitjat el boto play
-                audio.getPlayer().play(); //reproduim l'àudio
-            } else if (gestorEsdeveniments.equals(vista.getStop())) {
-                //Si hem pitjat el boto stop
-                audio.getPlayer().stop(); //parem la reproducció de l'àudio
-            } else if (gestorEsdeveniments.equals(vista.getPausa())) {
-                //Si hem pitjat el boto stop
-                audio.getPlayer().pause(); //pausem la reproducció de l'àudio
-            } else if (gestorEsdeveniments.equals(vista.getContinuar())) {
-                //Si hem pitjat el boto stop
-                audio.getPlayer().resume(); //continuem la reproducció de l'àudio
+    private void afegirDades() {
+       JComboBox boxAlbum =  vista.getjBoxAlbum();
+       boxAlbum.removeAllItems();
+       //List<String> listaAlbums = new ArrayList<>;
+       
+       boxAlbum.addItem("Todo");
+        
+    }
+
+    class ControladorBotones implements ActionListener {
+
+        //Dotem de funcionalitat als botons
+        public void actionPerformed(ActionEvent esdeveniment) {
+            //Declarem el gestor d'esdeveniments
+            Object gestorEsdeveniments = esdeveniment.getSource();
+            try {
+                if (gestorEsdeveniments.equals(vista.getPlay())) { //Si hem pitjat el boto play
+                    audio.getPlayer().play(); //reproduim l'àudio
+                } else if (gestorEsdeveniments.equals(vista.getStop())) {
+                    //Si hem pitjat el boto stop
+                    audio.getPlayer().stop(); //parem la reproducció de l'àudio
+                } else if (gestorEsdeveniments.equals(vista.getPausa())) {
+                    //Si hem pitjat el boto stop
+                    audio.getPlayer().pause(); //pausem la reproducció de l'àudio
+                } else if (gestorEsdeveniments.equals(vista.getContinuar())) {
+                    //Si hem pitjat el boto stop
+                    audio.getPlayer().resume(); //continuem la reproducció de l'àudio
+                }
+            } catch (BasicPlayerException e) {
+                e.printStackTrace();
             }
-        } catch (BasicPlayerException e) {
-            e.printStackTrace();
         }
     }
 }
