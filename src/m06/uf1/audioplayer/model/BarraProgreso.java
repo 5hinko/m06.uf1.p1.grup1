fd8c3ae9f0d72da7637c41a72aa900da6ce55b7b
5hinko <se4bzhang@gmail.com>
2019-05-02 21:50:44 +0200

    Prueba Con La barra de Progreso

--- src/m06/uf1/audioplayer/model/BarraProgreso.java
+++ src/m06/uf1/audioplayer/model/BarraProgreso.java
@@ -7,7 +7,9 @@ package m06.uf1.audioplayer.model;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.JScrollBar;
+import javax.swing.SwingUtilities;
 import sun.misc.VM;
 
 /**
@@ -17,23 +19,24 @@ import sun.misc.VM;
 public class BarraProgreso extends Thread {
 
     private JScrollBar barraProgreso;
+    private JLabel textoProgreso;
     private int numProgress = 0;
     private boolean start;
     private boolean pause;
     private boolean stop;
 
-    public BarraProgreso(Object barra) {
+    public BarraProgreso(Object barra, Object progreso) {
         this.barraProgreso = (JScrollBar) barra;
+        this.textoProgreso = (JLabel) progreso;
     }
 
     @Override
     public void run() {
         super.run(); //To change body of generated methods, choose Tools | Templates.
-
+        System.out.println("Hilo Escucha");
         do {
             //Start
             while (start) {
-
                 int progresoNum = barraProgreso.getValue();
                 int limitProgreso = barraProgreso.getMaximum();
                 for (numProgress = progresoNum; numProgress <= limitProgreso || !stop; numProgress++) {
@@ -44,17 +47,14 @@ public class BarraProgreso extends Thread {
                     } catch (Exception e) {
                     }
 
-                    synchronized (barraProgreso) {
-                        barraProgreso.setValue(numProgress);
-                    }
                     // Runs inside of the Swing UI thread
-                    /*
                     SwingUtilities.invokeLater(new Runnable() {
                         public void run() {
-                            barraProgreso.setValue(i);
+                            System.out.println("Hilo Start");
+                            textoProgreso.setText((progresoNum / 60) + ":" + (progresoNum % 60));
+                            barraProgreso.setValue(numProgress);
                         }
                     });
-                     */
 
                     //Pausa
                     if (pause) {
@@ -66,6 +66,7 @@ public class BarraProgreso extends Thread {
                     }
                 }
             }
+            todosMismoVariable(false);
             //reset
         } while (1 == 1);
         //System.out.println("No Me dejes morir!");
@@ -89,14 +90,14 @@ public class BarraProgreso extends Thread {
 
     public void itsStop() {
         start = false;
+        itsContinuar();
         stop = true;
-        try {
-            wait(10);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(BarraProgreso.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        todosMismoVariable(false);
-        barraProgreso.setValue(0);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                barraProgreso.setValue(0);
+            }
+        });
     }
 
     public void itsPause() {
@@ -104,12 +105,18 @@ public class BarraProgreso extends Thread {
     }
 
     public void itsContinuar() {
-        notify();
         pause = false;
+        notify();
     }
 
     public void posicionBarra(float num) {
         numProgress = (int) num;
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                barraProgreso.setValue(numProgress);
+            }
+        });
     }
 
 }
